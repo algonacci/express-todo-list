3a5d34907d2d9f6abf381fbd883e3266ef752476
Eric Julianto <algonacci1@gmail.com>
2023-02-01 19:19:29 +0700

    Initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+
+app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+module.exports = app;
