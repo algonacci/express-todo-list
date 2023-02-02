d3b471851905cf615bdc0ce809c0a1a8ebe4752a
Eric Julianto <algonacci1@gmail.com>
2023-02-02 11:47:00 +0700

    get task

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const path = require("path");
 const express = require("express");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "todo-list-php",
+});
 
 app = express();
 
@@ -7,7 +15,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", "./public");
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  connection.query("SELECT * FROM todos", (error, results) => {
+    if (error) {
+      console.log(error);
+    }
+    res.render("index.ejs", { todos: results });
+  });
 });
 
 module.exports = app;
